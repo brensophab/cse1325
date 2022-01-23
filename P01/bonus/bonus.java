534a1b50eccea949f1419d8b6743dadcbf3b8fc5
Brendan Sophabmixay <brensophabmixay@gmail.com>
2022-01-23 00:27:59 -0600

    Bonus

--- P01/bonus/bonus.java
+++ P01/bonus/bonus.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 import java.util.*;
 class HelloWorld{
 	public static void main(String []args){
-	Scanner scan = new Scanner(System.in);
 	System.out.print("What is your name?");
+	Scanner scan = new Scanner(System.in);
+	
 	String name = scan.next();
-	System.out.printIn("Hello " + name);
+	System.out.println("Hello " + name);
 	}
 }
