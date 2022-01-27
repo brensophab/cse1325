da0eefa4980750237caeec65159f1b057dc37ff7
brensophab <brensophabmixay@gmail.com>
2022-01-27 01:41:24 -0600

    Removed unused library

--- P01/bonus/bonus.java
+++ P01/bonus/bonus.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
-import java.util.*;
 class HelloWorld{
 	public static void main(String []args){
 	System.out.print("What is your name?");
-	Scanner scan = new Scanner(System.in);
-	
-	String name = scan.next();
-	System.out.println("Hello " + name);
+	try (Scanner scan = new Scanner(System.in)) {
+		String name = scan.next();
+		System.out.println("Hello " + name);
+	}
 	}
 }
