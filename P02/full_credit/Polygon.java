c91a6accda70d1af2742fab3b0a9f8ec97239f29
Brendan Sophabmixay <brensophabmixay@gmail.com>
2022-01-31 22:45:24 -0600

    commit 2

--- P02/full_credit/Polygon.java
+++ P02/full_credit/Polygon.java
@@ -1,16 +1,40 @@
 import java.util.*;
-class Polygon(){
-	public static void main(String args[])
-	{
+import java.util.Scanner;
+public class Polygon(){
 	private int side;
 	private double perimeter;
-	for(int i=0; i< side.length;i++){
-		System.out.println("Side length(0 when done)");
+	
+	public double addSide(double length)//while getsides iterates, increment value(size) by one
+	{
+	
+	getSides();
+	}
+	public getSides()
+	{
+		do //iterates once and then passes value of length into getSides(i hope)
+		{
 		Scanner in = new Scanner(System.in);
-		side = in.nextInt();
-		i++;
+		length = in.nextInt();
+		System.out.println("Side length(0 when done):");
+		return length;	
+		}while(input!=0);
+	
+	}
+	public getPerimeter()
+	{
+	System.out.println("Perimeter of ""-sided polygon is " + addSide());
 	}
 	
-	
-	}
+	public static void main(String args[])
+{
+	 
+		for(int i=0; i< side.length;i++)
+		{
+			getSides();
+			
+			i++;
+		
+		}
+		getPerimeter();
+		System.exit(0);
 }
