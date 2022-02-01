30a47e542470fe86be8c0e3d579a46682cab9d1c
Brendan Sophabmixay <brensophabmixay@gmail.com>
2022-01-31 20:53:59 -0600

    Temp

---
+++ P02/full_credit/Polygon.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Polygon(){
+	public static void main(String args[])
+	{
+	private int side;
+	private double perimeter;
+	for(int i=0; i< side.length;i++){
+		System.out.println("Side length(0 when done)");
+		Scanner in = new Scanner(System.in);
+		side = in.nextInt();
+		i++;
+	}
+	
+	
+	}
+}
