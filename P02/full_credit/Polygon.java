5b8f4f100dc7fbfefafa4c4491e1b837781a4ec9
Brendan Sophabmixay <brensophabmixay@gmail.com>
2022-01-31 23:37:13 -0600

    Final commit i tried </3

--- P02/full_credit/Polygon.java
+++ P02/full_credit/Polygon.java
@@ -1,23 +1,28 @@
 import java.util.*;
 import java.util.Scanner;
-public class Polygon(){
+public class Polygon{
 	private int side;
 	private double perimeter;
 	
 	public double addSide(double length)//while getsides iterates, increment value(size) by one
 	{
 	double sum = 0;
-	for(int i=0 i <side.length;i++)
+	while(getSide()!=0)// replace second arg
 	{
 	sum +=getSides();
+	
 	}
+	return sum;
 	}
-	public getSides()
+	
+
+	
+	public int getSides()
 	{
 		 //iterates once and then passes value of length into getSides(i hope)
 		
 		Scanner in = new Scanner(System.in);
-		length = in.nextInt();
+		int length = in.nextInt();
 		System.out.println("Side length(0 when done):");
 		if(input==0)
 		{
@@ -25,24 +30,32 @@ public class Polygon(){
 		}
 		return length;	
 		
-		
-	
+			
 	}
-	public getPerimeter()
+	public int getPerimeter()
 	{
-	System.out.println("Perimeter of ""-sided polygon is " + addSide());
+	
+	return addSide();
 	}
+	
+
 	
 	public static void main(String args[])
 {
-	 
-		for(int i=0; i< side.length;i++)
+	 	int counter = 0;
+		while(getSides()!=0) // add counter for sides here replace 2nd arg
 		{
+		
 			getSides();
 			
-			i++;
-		
+			
+		if(getSides()>0)
+		{
+		 counter+=1;
+		}
 		}
 		getPerimeter();
+		System.out.println("Perimeter of "+counter+"-sided polygon is"+getPerimeter());
 		System.exit(0);
 }
+}// i tried D:
