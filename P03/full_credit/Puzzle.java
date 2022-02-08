8f6d48b522293a2a66070982dd0b2f534c8bb89d
brensophab <brensophabmixay@gmail.com>
2022-02-07 18:12:49 -0600

    P03 first commmit (change build.xml dir)

---
+++ P03/full_credit/Puzzle.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*
+import java.text.Normalizer;
+
+public class Quint{
+	public String compareGuess(String guess){
+	//For actual and check, letters will be replaced with spaces when found
+	Word actual = new Word(solution.toString());//puzzle solution
+	Word check  =  new Word(guess); 				//Guess at solution
+	Word result = new Word ("...."); 			//Result to be returned
+	
+	//Check for correct letter in correct position
+	for(int i=0; i<5; ++i) {
+		if(check.charAt(i)==actual.charAt(i)){
+				result.setChartAt(Character.toUpperCase(solution.charAt(i)), i);
+				check.setCharAt(' ', i);
+				actual.setCharAt(' ', i);
+				continue;
+			}
+		}
+		
+	}	
+		
+	
+	public static void main(String[] args){
+	System.out.println("Hello world!");
+	}
+	}
+
