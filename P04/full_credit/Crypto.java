91ebb4532ab22d23305efc1151d8b867fb8414c7
brensophab <brensophabmixay@gmail.com>
2022-02-11 12:03:40 -0600

    save 2/11/2022

---
+++ P04/full_credit/Crypto.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.security.MessageDigest;
+
+public class Crypto {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+
+    }
+    public Rot13(){
+        char[] s = args[];
+        for(int i=0;i<s.length;++i){
+            char c =s.charAt[i];
+            
+        }
+    }
+
+}
