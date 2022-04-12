53160fb2673f0a25caf4284ee227fd9c1fbe895b
brensophab <brensophabmixay@gmail.com>
2022-04-11 22:25:55 -0500

    Test 2

--- P09/Full_Credit/main.cpp
+++ P09/Full_Credit/main.cpp
@@ -1,9 +1,7 @@
 #include<iostream>
 #include "color.h"
 
-
-int main(){
-
+int main() {
     Color limegreen{137,235,52};
     Color violetish{235,52,52};
     Color brightpink{235,52,125};
@@ -11,4 +9,10 @@ int main(){
               << violetish.colorize("Stan Red Velvet") << ' '
               << brightpink.colorize("Pink") << "\n\n";
 
+    int red, green, blue;
+    std::cout << "Enter rgb values\n"; 
+    std::cin >> red >> green >> blue;
+    Color color{red, green, blue};
+    std::cout << color.colorize(color.to_string()) << std::endl;
 }
+
