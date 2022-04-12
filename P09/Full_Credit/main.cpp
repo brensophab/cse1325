0491eec8a68ecea8d5b0526efdcbe4bcc6152b8b
Brendan Sophabmixay <bxs5157@mavs.uta.edugit>
2022-04-11 22:44:18 -0500

    Another test commit

--- P09/Full_Credit/main.cpp
+++ P09/Full_Credit/main.cpp
@@ -1,18 +1,17 @@
-#include<iostream>
 #include "color.h"
+#include <iostream>
 
 int main() {
-    Color limegreen{137,235,52};
-    Color violetish{235,52,52};
-    Color brightpink{235,52,125};
-    std::cout << limegreen.colorize("Green") << ' '
-              << violetish.colorize("Stan Red Velvet") << ' '
-              << brightpink.colorize("Pink") << "\n\n";
+    Color uta_blue{0,100,177};
+    Color uta_orange{245,128,38};
+    Color maroon{93,23,37};
+    std::cout << uta_blue.colorize("UTA Blue") << ' '
+              << uta_orange.colorize("UTA Orange") << ' '
+              << maroon.colorize("Maroon") << "\n\n";
 
     int red, green, blue;
-    std::cout << "Enter rgb values\n"; 
+    std::cout << "Enter red, green, and blue ints:   "; 
     std::cin >> red >> green >> blue;
     Color color{red, green, blue};
     std::cout << color.colorize(color.to_string()) << std::endl;
 }
-
